c15f52943e82bc4cd3833a16e3b2a280fc429fdd
Alessandro Emanuel <alesefs@hotmail.com>
2024-03-22 18:23:01 -0300

    update xcustomtagText

--- app/src/main/java/com/example/introtocompose/CustomTextTag.kt
+++ app/src/main/java/com/example/introtocompose/CustomTextTag.kt
@@ -12,24 +12,37 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DrawerDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 
 @Composable
-fun CustomTextTag(
+fun CustomTextTag1(
     text: String,
     backgroundColor: Color,
     borderColor: Color,
@@ -48,7 +61,6 @@ fun CustomTextTag(
         ,
         text = text,
         color = contentColor,
-//        style = TextStyle(),
         maxLines = 1,
         overflow = TextOverflow.Ellipsis,
         fontSize = 12.sp,
@@ -59,20 +71,53 @@ fun CustomTextTag(
     )
 }
 
+@Composable
+fun CustomTextTag2(
+    text: String,
+    backgroundColor: Color,
+    borderColor: Color,
+    contentColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        color = contentColor,
+        style = TextStyle(
+            fontSize = 12.sp,
+            lineHeight = 12.sp,
+            textAlign = TextAlign.Center,
+        ),
+        modifier = modifier
+            .drawBehind {
+                drawRoundRect(
+                    Color(backgroundColor.value),
+                    cornerRadius = CornerRadius(999.dp.toPx())
+                )
+            }
+            .border(1.dp, borderColor, RoundedCornerShape(size = 999.dp))
+            .padding(vertical = 2.dp, horizontal = 8.dp)
+            .wrapContentWidth()
+            .height(16.dp),
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        softWrap = true,
+    )
+}
+
 @Preview
 @Composable
 fun CustomTextPreview() {
     IntroToComposeTheme {
         Column {
-            CustomTextTag(
+            CustomTextTag1(
                 text = "Joao Joao Jose",
                 backgroundColor = Color.Cyan,
                 borderColor = Color.White,
                 contentColor = Color.White
             )
             Spacer(modifier = Modifier.size(8.dp))
-            CustomTextTag(
-                text = "Joao Joao Jose".uppercase(),
+            CustomTextTag2(
+                text = "Joao Joao Jose",
                 backgroundColor = Color.Green,
                 borderColor = Color.Yellow,
                 contentColor = Color.Yellow
