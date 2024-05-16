38537106b1ec3c8b86928a5c0b90549be83def9e
Alessandro Emanuel <alesefs@hotmail.com>
2024-05-15 22:54:19 -0300

    update project

--- app/src/main/java/com/example/introtocompose/CustomCardView.kt
+++ app/src/main/java/com/example/introtocompose/CustomCardView.kt
@@ -60,15 +60,6 @@ sealed class CustomCardViewStyle {
     ) : CustomCardViewStyle()
 }
 
-
-object Colors {
-    val Teal = Color(red = 0, green = 220, blue = 220)
-    val TealUlong = Color(0xFF00DCDC)
-    val Petroleum = Color(red = 0, green = 80, blue = 80)
-    val PetroleumUlong = Color(0xFF005050)
-    val LightBlue = Color(0xFF005599)
-}
-
 object CustomCardView {
     object Header
     object Center
---
+++ app/src/main/java/com/example/introtocompose/utils/Colors.kt
@@ -0,0 +1,11 @@
+package com.example.introtocompose.utils
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val Teal = Color(red = 0, green = 220, blue = 220)
+    val TealUlong = Color(0xFF00DCDC)
+    val Petroleum = Color(red = 0, green = 80, blue = 80)
+    val PetroleumUlong = Color(0xFF005050)
+    val LightBlue = Color(0xFF005599)
+}
