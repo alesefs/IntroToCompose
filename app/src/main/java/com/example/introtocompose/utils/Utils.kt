977d60a1d5dccf8a6aa42c0976457cf8006cdf18
Alessandro Emanuel <alesefs@hotmail.com>
2024-05-17 10:25:14 -0300

    usando when em sealed class in compose

---
+++ app/src/main/java/com/example/introtocompose/utils/Utils.kt
@@ -0,0 +1,17 @@
+package com.example.introtocompose.utils
+
+/*
+@SuppressLint("StateFlowValueCalledInComposition")
+
+val type = remember {
+    MutableStateFlow<Sealed_Class>(Sealed_Class.SubClass())
+}
+
+//uso
+type.value = when(text) {
+  "X" -> Sealed_Class.X()
+  "Y" -> Sealed_Class.Y()
+  "Z" -> Sealed_Class.Z()
+  else -> Sealed_Class.X()
+}
+ */
